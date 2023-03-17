be7c0b21b5304a76d2cc083a06c096992854b2e2
Michał Gniadek <mgniadek@interia.pl>
2023-03-17 23:21:40 +0100

    Dynamic date

--- siter.py
+++ siter.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import os
+import datetime
 from imager import remap_img
 from jinja2 import Environment, FileSystemLoader
 
@@ -12,10 +13,15 @@ talerz_image.decode_content = True
 
 img = remap_img(talerz_image)
 
-os.makedirs("_site")
+today = datetime.datetime.today()
+monday = today - datetime.timedelta(days=today.weekday())
+sunday = monday + datetime.timedelta(days=6)
+
+os.makedirs("_site", exist_ok=True)
 img.save("_site/transformed.png")
 with open("_site/index.html", "w", encoding="utf-8") as f:
     env = Environment(loader=FileSystemLoader("templates"))
-    text = env.get_template("index.html").render(
-        emalia=data["emalia"], talerz_img="transformed.png")
+    text = env.get_template("index.html").render(date=f"{monday.strftime('%d.%m')}-{sunday.strftime('%d.%m')}",
+                                                 emalia=data["emalia"],
+                                                 talerz_img="transformed.png")
     f.write(text)
